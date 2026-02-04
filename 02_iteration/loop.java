12541492eb98cdabdf358afbe5632866bbfc27fc
Bikash-23 <paribikash23@gmail.com>
2026-02-04 16:48:06 +0530

    java

---
+++ 02_iteration/loop.java
@@ -0,0 +1,95 @@
+import java.util.Scanner;
+
+public class loop{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        // int n,i=1;
+        // n = sc.nextInt();
+        // while(i<=n){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        // int n,i=1;
+        // n = sc.nextInt();
+        // while(n>=i){
+        //     System.out.println(n);
+        //     n--;
+        // }
+        // int n,i=1;
+        // n = sc.nextInt();
+        // int sum = 0;
+        // while(i<=n){
+        //     sum+=i;
+        //     i++;
+        // }
+        // System.out.println(sum);
+
+        // int n;
+        // n = sc.nextInt();
+        // for(int i = 0;i<n;i++){
+        //     for(int j = 0;j<n;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // int num  = 123;
+        // int temp = num;
+        // int rev = 0;
+        // while(temp >0){
+        //    int remainder = temp%10;
+        //    rev = rev*10 + remainder;
+        //    temp /=10;
+        // }
+        // System.out.println(rev);
+
+        // int n = 1;
+        // do{
+        //     System.out.println("Hello Ji "+ n);
+        //     n++;
+        // }while( n <= 5);
+
+        // break :: to exit the loop 
+         
+        // while(true){
+        //     System.out.print("Enter Number: ");
+        //     int n = sc.nextInt();
+        //     if(n %10 == 0)
+        //         break;
+        //     else
+       //         System.out.println("Number is: "+n);
+        // }
+
+        // continue :: to skip an iteration
+
+        // while(true){
+        //     System.out.print("Enter Number: ");
+        //     int n = sc.nextInt();
+        //     if(n%10 == 0){
+        //         continue;
+        //     }
+        //     if(n == 404){
+        //         break;
+        //     }
+        //     System.out.println("Print: "+n);
+        // }
+
+        // check prime
+        
+        // System.out.print("Enter Number: ");
+        // int n = sc.nextInt();
+        // boolean isPrime = true;
+     
+        // for(int i = 2;i*i < n;i++){
+        //     if(n% i == 0){
+        //         isPrime = false;
+        //         break;
+        //     }
+        // }
+        // if(isPrime == true){
+        //     System.out.println("Prime");
+        // }else{
+        //     System.out.println("Not Prime");
+        // }
+    }
+}
